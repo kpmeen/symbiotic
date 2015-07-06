92ee057aefad36bc2a6a61ad4e5e67e702490bad
Knut Petter Meen <kp@scalytica.net>
2015-07-06 09:03:39 +0200

    Very, very, very crude start on integrating upload functionality

--- src/main/scala/net/scalytica/symbiotic/css/AppCSS.scala
+++ src/main/scala/net/scalytica/symbiotic/css/AppCSS.scala
@@ -2,7 +2,7 @@ package net.scalytica.symbiotic.css
 
 import net.scalytica.symbiotic.components.dman.{FolderTree, FolderContent, FolderTreeItem, PathCrumb}
 import net.scalytica.symbiotic.components.{Spinner, Footer, LeftNav, TopNav}
-import net.scalytica.symbiotic.pages.{DocManagementPage, ItemsPage, LoginPage}
+import net.scalytica.symbiotic.pages.{HomePage, DocManagementPage, ItemsPage, LoginPage}
 
 import scalacss.Defaults._
 import scalacss.ScalaCssReact._
@@ -16,6 +16,7 @@ object AppCSS {
       Colors,
       Material,
       FontAwesome,
+      HomePage.Style,
       Spinner.Style,
       Footer.Style,
       TopNav.Style,
