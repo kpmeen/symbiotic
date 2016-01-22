da2b1798d32ed185512bfc4fe50fedd8c3b1ac55
Knut Petter Meen <kp@scalytica.net>
2016-01-22 19:25:45 +0100

    #2 and #31: Cleaning up signature for MemberRepository trait

--- server/app/services/project/MemberService.scala
+++ server/app/services/project/MemberService.scala
@@ -4,6 +4,7 @@
 package services.project
 
 import com.google.inject.{Inject, Singleton}
+import core.lib.SuccessOrFailure
 import models.base.Id
 import models.party.PartyBaseTypes.{OrganisationId, UserId}
 import models.project.{Member, MemberId, ProjectId}
@@ -12,7 +13,7 @@ import repository.MemberRepository
 @Singleton
 class MemberService @Inject() (val memberRepository: MemberRepository) {
 
-  def save(m: Member): Unit = memberRepository.save(m)
+  def save(m: Member): SuccessOrFailure = memberRepository.save(m)
 
   def findById(mid: MemberId): Option[Member] = memberRepository.findById(mid)
 
