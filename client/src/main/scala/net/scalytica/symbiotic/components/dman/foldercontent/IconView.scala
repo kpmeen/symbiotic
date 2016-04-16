e274920875941aecefecffe176eadac409e33f1c
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-04-16 18:17:33 +0200

    Making download links work in FF and Chrome. Plus fixing download button

--- client/src/main/scala/net/scalytica/symbiotic/components/dman/foldercontent/IconView.scala
+++ client/src/main/scala/net/scalytica/symbiotic/components/dman/foldercontent/IconView.scala
@@ -51,7 +51,7 @@ object IconView {
     override def renderFile(selected: Option[ManagedFile], contentType: FileTypes.FileType, wrapper: ManagedFile): ReactElement =
       <.div(Style.fcGrouping(selected.contains(wrapper)), ^.onClick --> setSelected(wrapper),
         <.i(FileTypes.Styles.Icon3x(contentType).compose(FolderContentStyle.file(true))),
-        <.a(^.id := wrapper.metadata.fid, ^.href := wrapper.downloadLink,
+        <.a(^.id := wrapper.metadata.fid, ^.onClick --> downloadFile(wrapper),
           <.span(Style.folderLabel,
             wrapper.filename,
             wrapper.metadata.lock.map { l =>
