bb3fdd13d6da35d47c7b782f69e7b34a9f1d56f7
Knut Petter Meen <kp@scalytica.net>
2017-01-03 12:28:11 +0100

    Pushing coverage data to codacy

--- shippable-ci.sh
+++ shippable-ci.sh
@@ -2,5 +2,10 @@
 
 cd server;
 
-sbt clean coverage test coverageReport
+if [[ -n "$CODACY_PROJECT_TOKEN" ]]; then
+  sbt clean coverage test coverageReport codacyCoverage;
+else
+  echo "Coverage reporting disabled for PR from forks";
+  sbt clean coverage test coverageReport;
+fi
 
