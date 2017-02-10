951106b405c9926913173bf8900a16c6c0d74731
Knut Petter Meen <kp@scalytica.net>
2017-02-10 13:58:03 +0100

    Playing around...enabling codacy integration.

--- gitlab-ci.sh
+++ gitlab-ci.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-docker login -u gitlab-ci-token -p $CI_BUILD_TOKEN registry.gitlab.com
+cd $CI_PROJECT_DIR/server;
 
-cd server;
-
-# if [ -n "$CODACY_PROJECT_TOKEN" ]; then
-#  echo "Building from upstream. Codacy reporting enabled.";
-#  sbt clean coverage test coverageReport codacyCoverage;
-#else
-#  echo "Building PR from fork. Codacy reporting disabled.";
+if [ -n "$CODACY_PROJECT_TOKEN" ]; then
+  echo "Building from upstream. Codacy reporting enabled.";
+  sbt clean coverage test coverageReport codacyCoverage;
+else
+  echo "Building PR from fork. Codacy reporting disabled.";
   sbt clean coverage test coverageReport;
-#fi
-
-sbt docker:publish
+fi
 
